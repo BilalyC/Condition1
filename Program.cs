cd646a96367499fee41f0cfad6bd616713d42a31
BilalyC <bilaly.camara@outlook.com>
2018-12-19 12:50:40 +0100

    Exercice 1 Complet

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean rainy = false;
+            if (rainy)
+                Console.WriteLine("Bonjour, il pleut aujourd’hui, vous devriez prendre votre parapluie.");
+            else
+                Console.WriteLine("Bonjour, il ne pleut pas aujourd’hui, bonne journée.");
+        }
+    }
+}
